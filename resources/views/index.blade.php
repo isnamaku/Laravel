d58fc2d2cc10a3539705c34ae9a284d553dd5283
isna_na <narutost9@gmail.com>
2020-08-05 11:41:18 +0700

    Static Web

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SanberBook</title>
+</head>
+<body>
+<h1>SanberBook</h1>
+<h2>Social Media Developer Santai Berkualitas</h2>
+<p>Belajar dan Berbagi agar hidup ini semakin santai berkualitas</p>
+<h3>Benefit Join di SanberBook</h3>
+
+<ul>
+	<li>Mendapatkan motivasi dari sesama developer</li>
+	<li>Sharing knowledge dari para mastah Sanber</li>
+	<li>Dibuat oleh calon web developer terbaik</li>
+</ul>
+<h3>Cara Bergabung ke SanberBook</h3>
+<ul>
+	<li>1. Mengunjungi Website ini</li>
+	<li>2. Mendaftar di <a href="register">Form Sign Up</a></li>
+	<li>3. Selesai</li>
+</ul>
+
+</body>
+</html>
---
+++ resources/views/welcome2.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SanberBook</title>
+</head>
+<body>
+<h1>Selamat Datang {{ $first_name. ' ' . $last_name }} !</h1>
+<h3>Terimakasih telah bergabung di SanberBook. Sosial media kita bersama!</h3>
+</body>
+</html>
